36f33073e317e6135b4a590cccbb8afccf8b9ec4
Andréia de Souza Ohe <andreiadesouzaohe@Andreias-MacBook-Air.local>
2017-08-15 17:23:38 -0700

    Implemented Injecting a service into another service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { CursosService } from './cursos/cursos.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -20,6 +21,7 @@ import { CriarCursoModule } from './criar-curso/criar-curso.module';
     CursosModule
   
   ],
+  providers: [LogService],
   // providers: [CursosService],
   bootstrap: [AppComponent]
 })
